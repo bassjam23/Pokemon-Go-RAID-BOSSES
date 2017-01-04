1994c9a59514ad8b9021bc005fed27a23921ae84
Cameron Smith <spellbee23@camerons-macbook-pro.local>
2017-01-03 23:05:05 -0700

    Initial Commit

---
+++ Pokemon Go IV Rater/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pokemon Go IV Rater
+//
+//  Created by Cameron Smith on 1/3/17.
+//  Copyright © 2017 Cameron Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
